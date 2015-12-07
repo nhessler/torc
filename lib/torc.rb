0859378b9c4a2974c6ee6ba50071c8e6045e9624
Josh Cheek <josh.cheek@gmail.com>
2015-12-07 14:14:34 -0700

    Remove ActiveSupport style indirection
    It is the same as just placing the instance methods directly into Torc,
    except it adds an additional node to the inheritance stack.

--- lib/torc.rb
+++ lib/torc.rb
@@ -2,28 +2,18 @@ require "torc/version"
 require "binding_of_caller"
 
 module Torc
-
-
-  def Torc.included(basename)
-    basename.class_eval do
-      include Torc::InstanceMethods
-    end
-  end
-
-  module InstanceMethods
-    def recurse(*args)
-      return [:recurse, args] if instance_variable_defined? :@caller
-      begin
-        @caller = binding.of_caller(1).eval('__method__')
-        caller_args = [:start, args]
-        loop do
-          caller_args = __send__ @caller, *caller_args.last
-          return caller_args unless caller_args.respond_to? :first
-          return caller_args unless caller_args.first == :recurse
-        end
-      ensure
-        remove_instance_variable :@caller if instance_variable_defined? :@caller
+  def recurse(*args)
+    return [:recurse, args] if instance_variable_defined? :@caller
+    begin
+      @caller = binding.of_caller(1).eval('__method__')
+      caller_args = [:start, args]
+      loop do
+        caller_args = __send__ @caller, *caller_args.last
+        return caller_args unless caller_args.respond_to? :first
+        return caller_args unless caller_args.first == :recurse
       end
+    ensure
+      remove_instance_variable :@caller if instance_variable_defined? :@caller
     end
   end
 end
