9c2567c6a5162228db601c0d0fe893ba896b4b80
Josh Cheek <josh.cheek@gmail.com>
2015-12-07 14:12:28 -0700

    Remove dependency on singleton variable MethodStack@depth

--- spec/torc_spec.rb
+++ spec/torc_spec.rb
@@ -7,26 +7,21 @@ describe Torc do
   let(:num)      { rand(100..200) }
 
   it 'has a version number' do
-    expect(Torc::VERSION).not_to be nil
+    expect(Torc::VERSION).to be_a_kind_of String
   end
 
   describe '#recurse' do
     it "doesn't alter the answer of the original method" do
-      MethodStack.reset_depth
       expect(recursed.factorial(1,20)).to eql(base.factorial(1,20))
     end
 
     it "keeps the stack from growing" do
-      MethodStack.reset_depth
       base.factorial(1, num)
-      base_depth = MethodStack.max_depth
-
-      MethodStack.reset_depth
       recursed.factorial(1, num)
-      recursed_depth = MethodStack.max_depth
 
       recursed_max_stack_depth = 5
-      expect(recursed_depth).to eql(base_depth - (num - recursed_max_stack_depth))
+      expected_depth = base.max_depth - (num - recursed_max_stack_depth)
+      expect(recursed.max_depth).to eql(expected_depth)
     end
 
     it "can solve problems that would normally cause stack overflows" do
