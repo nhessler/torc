712e4bf86764249ecf413ddee6362ddd88d21ce9
Josh Cheek <josh.cheek@gmail.com>
2015-12-07 18:10:40 -0700

    Supports nested / cross invocations

--- lib/torc.rb
+++ lib/torc.rb
@@ -2,18 +2,54 @@ require "torc/version"
 require "binding_of_caller"
 
 module Torc
-  def recurse(*args)
-    return [:recurse, args] if instance_variable_defined? :@caller
-    begin
-      @caller = binding.of_caller(1).eval('__method__')
-      caller_args = [:start, args]
-      loop do
-        caller_args = __send__ @caller, *caller_args.last
-        return caller_args unless caller_args.respond_to? :first
-        return caller_args unless caller_args.first == :recurse
-      end
-    ensure
-      remove_instance_variable :@caller if instance_variable_defined? :@caller
+  def self.extended(obj)
+    obj.__send__ :torc_init
+  end
+
+  def initialize(*args)
+    torc_init
+    super
+  end
+
+  def tail_call(name, *args)
+    if @_torc_state[:loop_stack].empty?
+      recurse name, *args
+    else
+      @_torc_state[:swap_to] = [name, args]
+      @_torc_state[:finished] = false
     end
+  end
+
+  def recurse(*args)
+    name = binding.of_caller(1).eval('__method__')
+    if @_torc_state[:loop_stack].last == name
+      @_torc_state[:finished] = false
+      return args
+    else
+      @_torc_state[:loop_stack].push name
+      begin
+        begin
+          @_torc_state[:finished] = true
+          args = __send__ name, *args
+          if @_torc_state[:swap_to]
+            name, args = @_torc_state[:swap_to]
+            @_torc_state[:swap_to] = nil
+          end
+        end until @_torc_state[:finished]
+        return args
+      ensure
+        @_torc_state[:loop_stack].pop
+      end
+    end
+  end
+
+  private
+
+  def torc_init
+    @_torc_state = {
+      loop_stack: [],
+      swap_to:    nil,
+      finished:   true,
+    }
   end
 end
--- spec/torc_spec.rb
+++ spec/torc_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe Torc do
+RSpec.describe Torc do
   let(:base)     { Base.new }
   let(:recursed) { Recursed.new }
   let(:torced)   { Torced.new }
@@ -19,14 +19,91 @@ describe Torc do
       base.factorial(1, num)
       recursed.factorial(1, num)
 
-      recursed_max_stack_depth = 5
+      recursed_max_stack_depth = 3
       expected_depth = base.max_depth - (num - recursed_max_stack_depth)
       expect(recursed.max_depth).to eql(expected_depth)
     end
 
     it "can solve problems that would normally cause stack overflows" do
       double_stack_size = `ulimit -s`.to_i * 2
-      expect(recursed.simple(double_stack_size)).to eql(0)
+
+      expect { base.simple(double_stack_size) }
+        .to(raise_exception(SystemStackError))
+
+      expect(recursed.simple(double_stack_size)).to eq(0)
     end
+
+    it 'can apply recursion to multiple methods' do
+      multiplier = Class.new {
+        include Torc
+
+        def double(n, acc=0)
+          return acc if n <= 0
+          recurse(n-1, acc+2)
+        end
+
+        def triple(n, acc=0)
+          return acc if n <= 0
+          recurse(n-1, acc+3)
+        end
+      }.new
+
+      expect(multiplier.double(10)).to eq(20)
+
+      expect(multiplier.triple( 9)).to eq(27)
+      expect(multiplier.triple(10)).to eq(30)
+      expect(multiplier.triple(11)).to eq(33)
+    end
+
+    it 'can call one recursive method from another' do
+      actual = Class.new {
+        include Torc
+
+        def cascading_sum(n, acc=0)
+          return acc if n <= 0
+          recurse(n-1, acc+sum(n))
+        end
+
+        def sum(n, acc=0)
+          return acc if n <= 0
+          recurse(n-1, acc+n)
+        end
+      }.new.cascading_sum(3)
+
+      expected = 3+2+1 +
+                   2+1 +
+                     1
+
+      expect(actual).to eq(expected)
+    end
+
+
+    it 'can support recursive methods that call back and forth from each other' do
+      actual = Class.new {
+        include Torc
+        def method1(n, acc=0)
+          return acc if n <= 0
+          tail_call :method2, n-1, acc+n
+        end
+        def method2(n, acc=0)
+          return acc if n <= 0
+          tail_call :method1, n-1, acc+n
+        end
+      }.new.method1(6)
+
+      expect(actual).to eq(6+5+4+3+2+1)
+    end
+
+    it 'can be extended onto an object' do
+      obj = Object.new.extend(Torc)
+      def obj.double(n, acc=0)
+        return acc if n <= 0
+        recurse(n-1, acc+2)
+      end
+      expect(obj.double(5)).to eq(10)
+    end
+
+    it 'is unaffected by overriding initialize'
+    it 'does not affect the superclass initialize'
   end
 end
